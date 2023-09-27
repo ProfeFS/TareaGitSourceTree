53643f01e605b5a86bf63a46d2c9302941c62336
Mireia Parra <miahigurashi02@gmail.com>
2023-09-27 18:19:07 +0200

    Resuelto back

--- src/tareas_git/Principal.java
+++ src/tareas_git/Principal.java
@@ -7,13 +7,8 @@ public class Principal {
 		 * Esto es un ejemplo de ejecución de una tarea.
 		 * No debe ser borrado. Es parte del programa.
 		 */
-		System.out.println("El resultado de la suma es: " + TareaEjemplo.ejecutaSuma());
-<<<<<<< HEAD
-		Dados_aleatorios.dado();
-=======
-		
-<<<<<<< HEAD
-=======
+
+
 
 		/*
 		 * Agragar vuestros nombres uno debajo del otro.
@@ -23,21 +18,12 @@ public class Principal {
 		 * Adrián
 		 * Javier
 		 * Miriam
-<<<<<<< HEAD
-<<<<<<< HEAD
-		 * 
-		 * 
 		 * Jorge
-=======
->>>>>>> 69da54729523518b3234d847c9d0f81320171a53
->>>>>>> 4dcbd95198df48d3eb67e5885c04825d49c095af
-=======
 		 * Juan
->>>>>>> 3a936df4ccdb16ecd306be5ab93a1eaa94db2efd
+		 * Mireia
 		 */
 
->>>>>>> e2c01ae69f9d1802d5548189ed0fcf35547eb3f2
->>>>>>> 316355e47f67d38e290fb08778d10ae82f8842d8
+
 	}
 
 }
