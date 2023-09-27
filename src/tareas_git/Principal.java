c5d029ac960eed52d57e12c2b6a251c5b7c73f3f
Erik <erikmora2002@hotmail.com>
2023-09-27 18:38:39 +0200

    nombre agregado

--- src/tareas_git/Principal.java
+++ src/tareas_git/Principal.java
@@ -8,14 +8,13 @@ public class Principal {
 		 * No debe ser borrado. Es parte del programa.
 		 */
 
-<<<<<<< HEAD
+
 		System.out.println("El resultado de la suma es: " + TareaEjemplo.ejecutaSuma());
 
 		Dados_aleatorios.dado();
 
 
-=======
->>>>>>> 9a537d2652a0354867270f51c5b804a3cbbf9d93
+
 		/*
 		 * Agragar vuestros nombres uno debajo del otro.
 		 * Gynny
